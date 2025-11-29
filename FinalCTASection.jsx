4c3312529eaab16d78d3493c6fed21a7b77eba6f
GabrielJaccoud <107004489+GabrielJaccoud@users.noreply.github.com>
2025-11-29 11:25:02 -0300

    Fix image source attribute in FinalCTASection

--- FinalCTASection.jsx
+++ FinalCTASection.jsx
@@ -33,7 +33,7 @@ const FinalCTASection = () => {
           
           <div className="cta-image">
             <img 
-              src="/mostardinha_cta_novo.png" ovo.png" 
+              src="/mostardinha_cta_novo.png" 
 	              alt="Mostardinha" 
 	              className="cta-mostardinha"
 	              loading="lazy"
